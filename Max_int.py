c815c92034c191ccdf882b7ad35453a49f832692
Jong18 <jong18@ru.is>
2021-09-02 14:24:00 +0000

    first code

---
+++ Max_int.py
@@ -0,0 +1,10 @@
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int = 0
+
+while num_int >= 0:
+    
+
+
+print("The maximum is", max_int)    # Do not change this line
